e5e287ce0c46fd62d1120f71fb895b992cfbe5c2
Albert <st.krwlng@gmail.com>
2012-02-10 13:03:35 +0800

    Add the WCF Test Project

---
+++ BasicWCFClient/BasicWCFClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using ChessLaw;
+
+namespace BasicWCFClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EndpointAddress ep = new EndpointAddress("http://localhost:8012/HelloWorld/");
+            IChessAIService proxy = ChannelFactory<IChessAIService>.CreateChannel(new BasicHttpBinding(), ep);
+            ChessState state = new ChessState(); state.SetupNewGame();
+            proxy.GameStart();
+            Console.WriteLine(proxy.GetStrategy(state, true));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BasicWCFTrial/BasicWCFTrial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using TD_0_WhiteService;
+
+namespace BasicWCFTrial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(ChessAIService));
+            host.AddServiceEndpoint(typeof(IChessAIService), new BasicHttpBinding(), "http://localhost:8012/HelloWorld/");
+            host.Open();
+            Console.WriteLine("Service Start");
+            Console.ReadLine();
+        }
+    }
+}
